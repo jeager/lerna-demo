daddac73e75765e279adaf716ac630bb5524c656
Vinicius Franco <viniciusf.arantes@gmail.com>
2020-07-08 20:37:23 -0300

    Adicionando estilo ao button

--- packages/shared-components/button/src/index.jsx
+++ packages/shared-components/button/src/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const Button = ({ children }) => {
+const Button = ({ children, color }) => {
   return (
     <button style={{
       textDecoration: 'none',
       color: '#fff',
-      backgroundColor: '#26a69a',
+      backgroundColor: color || '#26a69a',
       textAlign: 'center',
       letterSpacing: '.5px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      border: 'none',
+      borderRadius: '2px',
+      lineHeight: '36px',
+      padding: '0 16px',
+      textTransform: 'uppercase'
     }}>
       {children}
     </button>
